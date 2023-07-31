d3859fe26b30fa7eec7cdee1a0396a444ef159b3
Nikko Bansil <bjohnnikko@gmail.com>
2023-07-31 13:42:39 -0700

    render the left sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import {BiHomeCircle, BiUser} from 'react-icons/bi'
 import {BsBell, BsBookmark} from 'react-icons/bs'
 import {HiOutlineHashtag} from 'react-icons/hi'
@@ -36,7 +37,20 @@ const Home = () => {
       <div className="max-w-screen-lg w-full h-full flex relative">
         {/* left sidebar for navigation/header*/}
         <section className="fixed w-72 flex flex-col h-screen">
-
+          {
+            NAVIGATION_ITEMS.map((item)=>(
+              <Link
+                className="bg-white/50 flex items-center justify-center space-x-2 rounded-3xl p-4"
+                href={`/${item.title.toLocaleLowerCase()}`}
+                key={item.title}
+              >
+                <div>
+                  <item.icon />
+                </div>
+                <div>{item.title}</div>
+              </Link>
+            ))
+          }
         </section>
         <main>Home Timeline</main>
         <section>Right Section</section>
