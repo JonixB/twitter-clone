cf2aa4e38a94db64584b0f756bd936874590a46d
Nikko Bansil <bjohnnikko@gmail.com>
2023-07-31 13:15:54 -0700

    styling home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
+import {BiHomeCircle} from 'react-icons/bi'
+
+const NAVIGATION_ITEMS = [
+  {
+    title: 'Home',
+    icon: BiHomeCircle
+  }
+]
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-lg w-full h-full flex relative">
+        {/* left sidebar for navigation/header*/}
+        <section className="fixed w-72 flex flex-col h-screen">
+
+        </section>
+        <main>Home Timeline</main>
+        <section>Right Section</section>
+      </div>
+    </div>
   )
 }
 
