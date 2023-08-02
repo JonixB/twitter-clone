09898c6f2db394bc3b88472b3acf4a935a2331d6
Nikko Bansil <bjohnnikko@gmail.com>
2023-08-02 16:40:05 -0700

    adjust left nav design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,16 +34,16 @@ const NAVIGATION_ITEMS = [
 const Home = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-screen-lg w-full h-full flex relative">
+      <div className="max-w-screen-xl w-full h-full flex relative">
         {/* left sidebar for navigation/header*/}
-        <section className="fixed w-72 flex flex-col h-screen">
-          <Link href={'/'}>
+        <section className="fixed w-72 flex flex-col h-screen space-y-4 my-4">
+          <Link href={'/'} className='py-2 text-2xl'>
             <BsTwitter />
           </Link>
           {
             NAVIGATION_ITEMS.map((item)=>(
               <Link
-                className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl p-2"
+                className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl py-2 px-6"
                 href={`/${item.title.toLocaleLowerCase()}`}
                 key={item.title}
               >
