02b2a36d324a9cbf2fbd8834e65b4720402044c5
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-19 17:06:53 -0600

    start work on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,12 @@ const Home = () => {
     <div className="w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* left sidebar for navigation/header*/}
-        <LeftSidebar/>
-        {/*<main>Home Timeline</main>
-        <section>Right Section</section>*/}
+        <LeftSidebar />
+        <main className="ml-[275px] p-6 flex w-[600px] h-full min-h-screen flex-col border-l border-r border-gray-600">
+          <h1 className="text-2xl font-bold">Home</h1>
+        </main>
+        
+        {/*{<section>Right Section</section>*/}
       </div>
     </div>
   )
