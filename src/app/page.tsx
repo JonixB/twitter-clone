521ccd680a978697f5a02573acc1e361dfe8613f
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-19 15:29:16 -0600

    create and style lower button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import {BiHomeCircle, BiUser} from 'react-icons/bi'
 import {RiTwitterXFill} from 'react-icons/ri'
-import {BsBell, BsBookmark, BsTwitter, BsEnvelope} from 'react-icons/bs'
+import {BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots} from 'react-icons/bs'
 import {HiOutlineHashtag} from 'react-icons/hi'
 
 const NAVIGATION_ITEMS = [
@@ -44,7 +44,7 @@ const Home = () => {
             {
               NAVIGATION_ITEMS.map((item)=>(
                 <Link
-                  className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl py-2 px-6"
+                  className="hover:bg-white/20 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl py-2 px-6"
                   href={`/${item.title.toLocaleLowerCase()}`}
                   key={item.title}
                 >
@@ -55,12 +55,23 @@ const Home = () => {
                 </Link>
               ))
             }
-            <button className="rounded-full m-4 bg-primary py-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
+            <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
             Tweet
             </button>
           </div>
-          <button className="rounded-full m-4 bg-primary py-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
-            Tweet
+          <button className="rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-white/20 transition duration-200 w-full justify-between">
+            <div className="flex items-center space-x-2">
+              <div className="rounded-full bg-slate-400 w-10 h-10">
+
+              </div>
+              <div className="text-left text-sm">
+              <div className="font-semibold">Club of Coders</div>
+              <div className="text-xs">@clubofcoderscom</div>
+              </div>
+            </div>
+            <div>
+              <BsThreeDots/>
+            </div>
           </button>
         </section>
         {/*<main>Home Timeline</main>
