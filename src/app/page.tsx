d0aeceb9ac446edbc30ac708b550606467831745
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-20 16:43:22 -0600

    clean up tweeting box styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ const Home = () => {
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* left sidebar for navigation/header*/}
         <LeftSidebar />
-        <main className="ml-[275px] p-6 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-          <h1 className="text-xl font-bold">Home</h1>
-          <div className="border-t-[0.5px] border-b-[0.5px] border-gray-600 h-32 relative">
-            <div className="w-10 h-10 bg-slate-400 rounded-full"></div>
-            <div className="flex flex-col">
+        <main className="ml-[275px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+          <h1 className="text-xl font-bold p-6">Home</h1>
+          <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-4 space-x-2 border-gray-600 h-32 relative">
+            <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
+            <div className="flex flex-col w-full h-full">
               <input type="text" className="w-full h-full bg-transparent border-b-[0.5px] border-gray-600 p-4 outline-none border-none" placeholder="What's happening?" />
               <div className="w-full justify-between items-center flex">
                 <div>
