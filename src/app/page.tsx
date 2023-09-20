6a9af79eb082fe206e7ff2ae3f35262a8f020622
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-20 16:20:19 -0600

    add textbox for tweeting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,12 @@ const Home = () => {
           <h1 className="text-xl font-bold">Home</h1>
           <div className="border-t-[0.5px] border-b-[0.5px] border-gray-600 h-32 relative">
             <div className="w-10 h-10 bg-slate-400 rounded-full"></div>
-            <div className="flex flex-column"></div>
+            <div className="flex flex-col">
+              <div className="border-b-[0.5px]">
+                <input type="text" placeholder="What's happening?"/>
+              </div>
+              <div></div>
+            </div>
           </div>
         </main>
         
