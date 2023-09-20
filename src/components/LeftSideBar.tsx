f265b53b19fefc507fba2621e89c80cc52b87509
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-19 20:23:28 -0600

    -added padding

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -37,7 +37,7 @@ const NAVIGATION_ITEMS = [
 
 const LeftSidebar = () => {
   return (
-    <section className="fixed p-6 w-[275px] flex flex-col items-stretch h-screen">
+    <section className="fixed p-6 w-[275px] flex flex-col items-stretch h-screen px-6">
           <div className="flex flex-col items-stretch h-full space-y-4 mt4">
             {
               NAVIGATION_ITEMS.map((item)=>(
