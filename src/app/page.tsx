7f6002df2c3ebc47121ddf832fc09525a568a895
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-19 20:24:00 -0600

    finish styling main tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ const Home = () => {
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* left sidebar for navigation/header*/}
         <LeftSidebar />
-        <main className="ml-[275px] p-6 flex w-[600px] h-full min-h-screen flex-col border-l border-r border-gray-600">
-          <h1 className="text-2xl font-bold">Home</h1>
+        <main className="ml-[275px] p-6 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+          <h1 className="text-xl font-bold">Home</h1>
         </main>
         
         {/*{<section>Right Section</section>*/}
