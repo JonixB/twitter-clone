57dbd5e365eab9d4c57db7a7771f0aa4704bfcea
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-20 16:47:57 -0600

    initial design of tweet box complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,14 @@ const Home = () => {
         <LeftSidebar />
         <main className="ml-[275px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
           <h1 className="text-xl font-bold p-6">Home</h1>
-          <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-4 space-x-2 border-gray-600 h-32 relative">
+          <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-6 space-x-2 border-gray-600 relative">
             <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
             <div className="flex flex-col w-full h-full">
-              <input type="text" className="w-full h-full bg-transparent border-b-[0.5px] border-gray-600 p-4 outline-none border-none" placeholder="What's happening?" />
+              <input 
+                type="text" 
+                className="w-full h-full text-2xl placeholder:text-gray-600 bg-transparent border-b-[0.5px] border-gray-600 p-4 outline-none border-none" 
+                placeholder="What's happening?" 
+              />
               <div className="w-full justify-between items-center flex">
                 <div>
 
