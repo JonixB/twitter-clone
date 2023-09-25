b7765121d700847b9b0a25730b533cec2a5c8887
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-24 20:56:15 -0600

    make hover styling for searchbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,18 @@ const Home = () => {
         <MainComponent />
         <section className="w-[27%] sticky top-2 mt-2 flex flex-col items-stretch h-screen px-6">
           <div>
-            <div className="relative w-full h-full ">
-              <label htmlFor="searchBox"
-                className="absolute top-0 l-0 h-full flex items-center justify-center p-4"
-              >
-                <BsSearch className="w-5 h-5 text-gray-500" />
-              </label>
+            <div className="relative w-full h-full group">
               <input
                 id="searchBox"
                 type="text"
                 placeholder="Search Twitter"
-                className="outline-none focus:border-primary focus:border bg-neutral-900/90 w-full h-full rounded-xl py-4 pl-10 pr-14" />
+                className="outline-none peer focus:border-primary focus:border bg-neutral-900/90 w-full h-full rounded-xl py-4 pl-14 pr-4"
+              />
+              <label htmlFor="searchBox"
+                className="absolute top-0 l-0 h-full flex items-center justify-center p-4"
+              >
+                <BsSearch className="w-5 h-5" />
+              </label>
             </div>
           </div>
           <div></div>
