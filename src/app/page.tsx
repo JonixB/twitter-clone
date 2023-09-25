40bd9b5f3f1f7e722eeab41436128a8bd9fd0ed2
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-24 20:08:13 -0600

    style the twiter search box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/LeftSideBar"
 import MainComponent from "@/components/MainComponent"
+import { BsSearch } from "react-icons/bs"
 
 const Home = () => {
   return (
@@ -8,7 +9,16 @@ const Home = () => {
         <LeftSidebar />
         <MainComponent />
         <section className="fixed flex flex-col space-y-4">
-
+          <div>
+            <div className="relative w-full h-full">
+              <div className="absolute top-0 h-full flex items-center">
+                <BsSearch className="w-5 h-5 text-gray-500" />
+              </div>
+              <input type="text" placeholder="Search Twitter" className="w-full h-full rounded-xl p-4" />
+            </div>
+          </div>
+          <div></div>
+          <div></div>
         </section>
       </div>
     </div>
