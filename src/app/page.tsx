4662693d452db69da08b29f2fc6ef77074c83866
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-24 20:20:44 -0600

    fix positioning of right hand component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,20 @@ import { BsSearch } from "react-icons/bs"
 const Home = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-screen-xl w-full h-full flex relative">
+      <div className="max-w-screen-xl w-full h-full flex relative border-4">
         <LeftSidebar />
         <MainComponent />
-        <section className="fixed flex flex-col space-y-4">
+        <section className="border-4 flex flex-col items-stretch h-screen px-6">
           <div>
             <div className="relative w-full h-full">
-              <div className="absolute top-0 h-full flex items-center">
+              <label htmlFor="searchBox" className="absolute top-0 l-0 h-full flex items-center justify-center">
                 <BsSearch className="w-5 h-5 text-gray-500" />
-              </div>
-              <input type="text" placeholder="Search Twitter" className="w-full h-full rounded-xl p-4" />
+              </label>
+              <input
+                id="searchBox"
+                type="text"
+                placeholder="Search Twitter"
+                className="outline-none bg-transparent border-none  w-full h-full rounded-xl py-4 px-8" />
             </div>
           </div>
           <div></div>
