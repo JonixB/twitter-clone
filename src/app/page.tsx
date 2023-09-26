5a26cbfb9625735f040cc46a590e5aad71280ca7
Nikko Bansil <bjohnnikko@gmail.com>
2023-09-25 20:50:38 -0600

    adjust style of who to follow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ const Home = () => {
             <h3 className="font-bold text-xl my-2 px-4">What's Happening</h3>
             <div>
               {
-                Array.from({length:5}).map((_,i)=>(
+                Array.from({ length: 5 }).map((_, i) => (
                   <div key={i} className="hover:bg-white/10 p-4 last:rounded-b-xl transition duration-200">
                     <div className="font-bold text-lg">#trending {i + 1}</div>
                     <div className="text-xs text-neutral-400">50.2k</div>
@@ -41,10 +41,21 @@ const Home = () => {
             <h3 className="font-bold text-xl my-2 px-4">Who to follow</h3>
             <div>
               {
-                Array.from({length:5}).map((_,i)=>(
-                  <div key={i} className="hover:bg-white/10 p-4 last:rounded-b-xl transition duration-200">
-                    <div className="font-bold text-lg">#trending {i + 1}</div>
-                    <div className="text-xs text-neutral-400">50.2k</div>
+                Array.from({ length: 5 }).map((_, i) => (
+                  <div
+                    key={i}
+                    className="hover:bg-white/10 p-4 flex items-center last:rounded-b-xl transition duration-200"
+                  >
+                    <div className="w-10 h-10 bg-neutral-600 rounded-full"></div>
+                    <div className="flex flex-col space-y-2">
+                      <div>Other User</div>
+                      <div>@otheruser</div>
+                    </div>
+                    <div>
+                      <button className="rounded-full px-6 py-2 bg-white text-neutral-950">
+                        Follow
+                      </button>
+                    </div>
                   </div>
                 ))
               }
